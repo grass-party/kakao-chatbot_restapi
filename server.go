ed5bc5d942f44a9374d5418f7bb7f127d81e5a3c
Noh ByungKuk <raynear@gmail.com>
2018-05-02 14:58:25 +0900

    첫 커밋

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	//  "fmt"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+type Keyboard struct {
+	Type    string   `json:"type"`
+	Buttons []string `json:"buttons"`
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var aKey Keyboard
+	aKey.Type = "buttons"
+	aKey.Buttons = make([]string, 4)
+	aKey.Buttons[0] = "????1"
+	aKey.Buttons[1] = "????2"
+	aKey.Buttons[2] = "????3"
+	aKey.Buttons[3] = "????4"
+
+	jData, err := json.Marshal(aKey)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jData)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/keyboard", Index)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
